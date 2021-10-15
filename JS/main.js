d11792ffbd194c8f4a513780086bdc5085a38e7e
marashen <90586106+marashen@users.noreply.github.com>
2021-10-14 20:23:19 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,11 @@
-
+function dict() {
+  var ani = {
+    Species: "Dog",
+    Color: "Black",
+    Breed: "Labrador",
+    Age: "5",
+    Sound: "Bark!",
+    };
+    delete Animal.Sound;
+    document.getElementById("dicti").innerHTML = Animal.Sound;
+}
