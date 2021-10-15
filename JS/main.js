34a0d4d37240f1aae4f16dd4a556015d15c6294e
marashen <90586106+marashen@users.noreply.github.com>
2021-10-15 02:02:11 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,12 @@ function fsen() {
 	var p1 = "I ";
 	var p2 = "am ";
 	var p3 = "ali";
-	var p4 = "ve.":
+	var p4 = "ve.";
 	var wS = p1.concat(p2, p3, p4);
 	document.getElementById("conca").innerHTML = wS;
 }
 function sliceR() {
-	var sen = "If time is infinite are we alive?"
+	var sen = "If time is infinite are we alive?";
 	var sec = sen.slice(0,33);
 	document.getElementById("slice").innerHTML = sec;
 }
@@ -15,7 +15,7 @@ function stringM() {
 	var num = 115;
 	document.getElementById("string").innerHTML = num.toString();
 }
-Function priceM() {
-	var numx = 115.115115115
+function priceM() {
+	var numx = 115.115115115;
 	document.getElementById("price").innerHTML = numx.toPrecision(3);
 }
