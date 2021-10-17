8cda93f8aa6b178d7d52e0e2f7c2001ddc53b111
marashen <90586106+marashen@users.noreply.github.com>
2021-10-17 19:13:28 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,58 @@
-function fsen() {
-	var p1 = "I ";
-	var p2 = "am ";
-	var p3 = "ali";
-	var p4 = "ve.";
-	var wS = p1.concat(p2, p3, p4);
-	document.getElementById("conca").innerHTML = wS;
+var reset = 2;
+function clocksA() {
+	var light = document.getElementById("lightP").value;
+	document.getElementById("lightX").style.color = 'green';
+	document.getElementById("earthX").style.color = 'green';
+	document.getElementById("lightX").innerHTML = light;
+	document.getElementById("relativeX").style.color = 'green';
+	var lightD = parseFloat(light) / 100;
+	var earthT = 1;
+	reset = 1;
+	while (reset = 1) {
+		var lightS = earthT * 31557600;
+		var lqp1 = 299792458 * 299792458;
+		var lqp2 = lightD * 299792458;
+		var lqp3 = lqp2 * lqp2;
+		var lqp4 = lqp3 / lqp1;
+		var lqp5 = 1 - lqp4;
+		var lqp6 = Math.sqrt(lqp5);
+		var lqp7 = lightS * lqp6;
+		var lqpM = lqp7 / 60;
+		var lqpH = lqpM / 60;
+		var lqpD = lqpH / 24;
+		var lqpY = lqpD / 365;
+		var relLX = Math.floor(lqpY)
+		relativeX.innerHTML = relLX;
+		earthT++;
+		earthX.innerHTML = earthT;
+		if (isNaN(light)) {
+			reset = 0
+			document.getElementById("lightX").innerHTML = "Reenter Light %";
+			document.getElementById("lightX").style.color = 'red';
+			document.getElementById("earthX").innerHTML = "Reenter Light %";
+			document.getElementById("earthX").style.color = 'red';
+			document.getElementById("relativeX").innerHTML = "Reenter Light %";
+			document.getElementById("relativeX").style.color = 'red';
+		}
+		else if (light > 100 || light < 0) {
+			reset = 0
+			document.getElementById("lightX").innerHTML = "Reenter Light %";
+			document.getElementById("lightX").style.color = 'red';
+			document.getElementById("earthX").innerHTML = "Reenter Light %";
+			document.getElementById("earthX").style.color = 'red';
+			document.getElementById("relativeX").innerHTML = "Reenter Light %";
+			document.getElementById("relativeX").style.color = 'red';						}
+		}
+	}
 }
-function sliceR() {
-	var sen = "If time is infinite are we alive?";
-	var sec = sen.slice(0,33);
-	document.getElementById("slice").innerHTML = sec;
-}
-function stringM() {
-	var num = 115;
-	document.getElementById("string").innerHTML = num.toString();
-}
-function priceM() {
-	var numx = 115.115115115;
-	document.getElementById("price").innerHTML = numx.toPrecision(3);
+function clocksS() {
+	reset = 0
+	var earthT = 0
+	var lightS = 0
+	document.getElementById("lightX").innerHTML = "Reenter Light %";
+	document.getElementById("lightX").style.color = 'red';
+	document.getElementById("earthX").innerHTML = "Reenter Light %";
+	document.getElementById("earthX").style.color = 'red';
+	document.getElementById("relativeX").innerHTML = "Reenter Light %";
+	document.getElementById("relativeX").style.color = 'red';
 }
